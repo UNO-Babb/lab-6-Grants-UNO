31800ff64a3a1d4baf1dd01bb0f91f9d65750f72
Grants-UNO <grant.schaeffer@icloud.com>
2025-10-05 00:59:15 +0000

    Part 2

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,28 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Grant Schaeffer
+#Date: 10/4/25
+#Assignment: Word Count
 
 def main():
   textFile = open("gettysberg.txt", 'r')
-  
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+      letter = line.split()
+    for char in textFile:
+        if char.isalpha():
+          letterCount += 1
+    #print(line)
   
+  print("Lines: ", lineCount)
+  print("Words: ", wordCount)
+  print("Letters: ", letterCount)
 
 if __name__ == '__main__':
   main()
