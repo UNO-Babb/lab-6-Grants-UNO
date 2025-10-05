3c2ddaf3e41dd094623ac9517d4d5ba00e92cb42
Grants-UNO <grant.schaeffer@icloud.com>
2025-10-05 01:17:29 +0000

    Part 3

--- WordIndex.py
+++ WordIndex.py
@@ -1,19 +1,35 @@
 #WordIndex.py
-#Name:
-#Date:
-#Assignment:
+#Name: Grant Schaeffer
+#Date: 10/4/25
+#Assignment: Qord Index
 
 def main():
   textFile = open("fish.txt", 'r')
-  
+  linenum = 0
+
   words = {} #create an empty dictionary
-  
-  
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
+  for line in textFile:
+    linenum = linenum + 1
+    wordlist = line.split()
+    #print(wordlist)
+    for w in wordlist:
+      w = w.lower()
+      w = w.replace(",", "")
+      w = w.replace(".", "")
+      w = w.replace("!", "")
+
+      if w in words:
+        if linenum not in words[w]:
+          words[w].append(linenum)
+      else:
+        words[w] = [linenum]
+    for word in words:
+      print(word, words[word])
+  #print ("fish" in words) #is a word already in the dictionary?
+  #words["fish"] = [2]     #add a list to the dictionary
+  #print ("fish" in words) #is the word there now?
+  #words["fish"].append(5) #add to an existing list
+  #print(words)
 
 
 if __name__ == '__main__':
